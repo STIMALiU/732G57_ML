aa231ddc0e3ead2433654b924fc64127684913ac
josefwilzen <josefwilzen@gmail.com>
2025-09-04 10:13:35 +0200

    updates

--- labs/xgboost_code1.R
+++ labs/xgboost_code1.R
@@ -54,6 +54,7 @@ importance_matrix <- xgb.importance(model = xgb_model, feature_names = colnames(
 # Plot
 xgb.plot.importance(importance_matrix, top_n = 10, measure = "Gain")
 
+print(importance_matrix)
 
 
 # Prediktion på träningsdatan
@@ -75,52 +76,3 @@ mean(pred_class_val==Carseats_validation$High)
 # utan någon hyperparametersökning så får vi rätt bra värden på 
 #träffsäkerhet på valideringsdata
 
-
-
-#-------------------------------------------------------------------------------
-###
-tree.carseats <- tree(High ~ . - Sales, Carseats)
-###
-summary(tree.carseats)
-###
-plot(tree.carseats)
-text(tree.carseats, pretty = 0)
-###
-tree.carseats
-###
-set.seed(2)
-train <- sample(1:nrow(Carseats), 200)
-Carseats.test <- Carseats[-train, ]
-High.test <- High[-train]
-tree.carseats <- tree(High ~ . - Sales, Carseats,
-                      subset = train)
-tree.pred <- predict(tree.carseats, Carseats.test,
-                     type = "class")
-table(tree.pred, High.test)
-(104 + 50) / 200
-###
-set.seed(7)
-cv.carseats <- cv.tree(tree.carseats, FUN = prune.misclass)
-names(cv.carseats)
-cv.carseats
-###
-par(mfrow = c(1, 2))
-plot(cv.carseats$size, cv.carseats$dev, type = "b")
-plot(cv.carseats$k, cv.carseats$dev, type = "b")
-###
-prune.carseats <- prune.misclass(tree.carseats, best = 9)
-plot(prune.carseats)
-text(prune.carseats, pretty = 0)
-###
-tree.pred <- predict(prune.carseats, Carseats.test,
-                     type = "class")
-table(tree.pred, High.test)
-(97 + 58) / 200
-###
-prune.carseats <- prune.misclass(tree.carseats, best = 14)
-plot(prune.carseats)
-text(prune.carseats, pretty = 0)
-tree.pred <- predict(prune.carseats, Carseats.test,
-                     type = "class")
-table(tree.pred, High.test)
-(102 + 52) / 200
